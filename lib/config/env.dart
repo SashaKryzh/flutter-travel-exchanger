6d55a8613e14fd4fa19a03117fa6c022184891f0
Alexander <sashakryzh@gmail.com>
2024-07-06 09:29:50 +0200

    chore updates

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -5,7 +5,6 @@ abstract final class Env {
     await dotenv.load();
   }
 
-  static String get baseUrl => dotenv.env['BASE_URL'] ?? '';
   static String get supabaseUrl => dotenv.env['SUPABASE_URL'] ?? '';
   static String get supabaseAnonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? '';
 }
